603e1caae2c75b08e617524e3418b442c4a3e0c5
Dmitry Podkopaev <dev.elatero@gmail.com>
2020-11-14 18:54:23 +0300

    Updates

---
+++ precache-manifest.20f816028ca6edc46121d92e465e57a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bd26c4824d248a2c3df678471c63e8",
+    "url": "/post_project_hack/index.html"
+  },
+  {
+    "revision": "535898494c684887a892",
+    "url": "/post_project_hack/static/css/main.843f6038.chunk.css"
+  },
+  {
+    "revision": "97e1bfcd6559191a0124",
+    "url": "/post_project_hack/static/js/2.d9f432eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/post_project_hack/static/js/2.d9f432eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535898494c684887a892",
+    "url": "/post_project_hack/static/js/main.e1b3f380.chunk.js"
+  },
+  {
+    "revision": "c4727753adee07c40acd",
+    "url": "/post_project_hack/static/js/runtime-main.bd3bfb42.js"
+  }
+]);
